b3c50d0508414de379ab7807e7080716c4f30474
Sara896 <78496174+Sara896@users.noreply.github.com>
2021-02-15 17:37:47 +0100

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -47,7 +47,7 @@ ext <- c(0,20,35,55)  # xmin xmax ymin ymax
 copNDVI_Italy <- crop(copNDVI, ext)
 plot(copNDVI_Italy, col=clymax)
 
-
+#########deforestation example########
 library(RStoolbox)
  
 setwd("C:/lab/")
@@ -56,3 +56,41 @@ setwd("C:/lab/")
 
 
 defor1 <- brick("defor1_.jpg")  
+defor1
+
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+#
+defor2<- brick("defor2_.jpg")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+#####calculate 2 index, how they change in time########
+###   DVI  #####
+defor1#to see the name of the layers
+dvi1 <- defor1$defor1_.1 - defor1$defor1_.2
+dev.off()
+plot(dvi1)
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(dvi1, col=cl)
+#dvi after the cut
+dvi2 <- defor2$defor2_.1 - defor2$defor2_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi2, col=cl)
+
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+
+# difference biomass before cut and after cut, which mean the amount of energy lost in the ecosystem
+#hiher the value of the pixell, higher the loss
+difdvi <- dvi1 -dvi2
+dev.off()
+plot(difdvi)
+
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cldif, main="amount of energy lost!")
+
