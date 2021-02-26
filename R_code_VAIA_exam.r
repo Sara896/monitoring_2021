f4fe91d5e994e7958705a89b3cdfe52ac1f34014
Sara896 <78496174+Sara896@users.noreply.github.com>
2021-02-26 01:15:08 +0100

    Update R_code_VAIA_exam.r

--- R_code_VAIA_exam.r
+++ R_code_VAIA_exam.r
@@ -58,7 +58,7 @@ plot(N1018)
 
 
 #we want to zoom on our interested area, North-East Italy
-#to set the coordinates
+#do an array to set the coordinates
 ext<-c(7,14,44,48)
 
 #to crop the images
@@ -103,19 +103,23 @@ vfc1018<- crop(fc1018, ext)
 vfc1118<- crop(fc1118, ext)
 
 #a Ramppalette for the FCOVER
-cFCOVER<-colorRampPalette(c('beige', 'yellow', 'forestgreen'))(100)
+cFCOVER<-colorRampPalette(c('red', 'yellow', 'forestgreen'))(100)
 par(mfrow=c(1,2))
 plot(vfc1018, col=cFCOVER, main="FCover october 2018")
 plot(vfc1118, col=cFCOVER, main="FCover november 2018")
 
 
 
-#how the NDVIis is changed from october to november 2018
-difN<- vN1018-vN1118
+#how the NDVI is changed from october to november 2018
+# minus function subtract the value of each correspondent pixels in the 2 layers
+difN<- vN1018-vN1118 
 par(mfrow=c(1,2))
 cdif<- colorRampPalette(c('black','darkmagenta', 'pink', 'yellow', 'lightsalmon'))(100)
-plot(difN, col=cdif)
-hist(difN, col='green') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
+plot(difN, col=cdif) 
+#to do an istrogramm which plot the frequency of the values,how much pixel have that value?
+# xlab= to title the x axes
+hist(difN, col='springgreen', xlab= 'NDVI variation') 
+                                                      
 
 dev.off() # to cancel the graphs and them settings, let use create nez graphs with new settings
 
@@ -124,9 +128,9 @@ levelplot(difN) # to do graphs around the map showing the mean frequences of the
 diffc <- vfc1018-vfc1118
 par(mfrow=c(1,2))
 plot(diffc, col=cdif)
-hist(diffc, col='blue') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
+hist(diffc, col='blue', xlab= 'FCOVER variation') 
 dev.off()
-levelplot(diffc) # 
+levelplot(diffc)
 
 par(mfrow=c(3,2)) 
 plot(vN1018, col=cNDVI, main="NDVI october 2018")
@@ -147,108 +151,39 @@ plot(diffc, col=cdif, main="variazione FCover 2018")
 
 
 
-# we use raster function to charge the satellite images
+# we use raster function to upload the satellite images
 N1017<-raster("c_gls_NDVI_201710010000_GLOBE_PROBAV_V2.2.1.nc")
 N1117<-raster("c_gls_NDVI_201711110000_GLOBE_PROBAV_V2.2.1.nc")
-
-#to crop the images zooming on the Veneto region
-vN1017<-crop(N1017, ext)
-vN1117<-crop(N1117, ext)
-
-par(mfrow=c(1,2)) #3 rows and 2 columns
-plot(vN1017,col=cNDVI, main="NDVI october 2017")
-plot(vN1117,col=cNDVI, main="NDVI november 2017")
-
-#how the NDVIis is changed from october to november 2018
-difN17<- vN1017-vN1117
-par(mfrow=c(1,2))
-plot(difN17, col=cdif)
-hist(difN17, col='green') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
-dev.off()
-levelplot(difN17) 
-
-
-#to see the differences in cover vegetation
 fc1017<- raster("c_gls_FCOVER_201710030000_GLOBE_PROBAV_V1.5.1.nc")
 fc1117<- raster("c_gls_FCOVER_201711230000_GLOBE_PROBAV_V1.5.1.nc")
 
+#to crop the images 
+vN1017<-crop(N1017, ext)
+vN1117<-crop(N1117, ext)
 vfc1017<- crop(fc1017, ext)
 vfc1117<- crop(fc1117, ext)
 
-par(mfrow=c(1,2))
-plot(vfc1017, col=cFCOVER, main="FCover october 2017")
-plot(vfc1117,col=cFCOVER, main="FCover november 2017")
-
-diffc17 <- vfc1017-vfc1117
-par(mfrow=c(1,2))
-plot(diffc17, col=cdif)
-hist(diffc17, col='green') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
-dev.off()
-levelplot(diffc17) 
-
-par(mfrow=c(3,2)) #3 rows and 2 columns
+par(mfrow=c(2,2)) #2 rows and 2 columns
 plot(vN1017,col=cNDVI, main="NDVI october 2017")
 plot(vN1117,col=cNDVI, main="NDVI november 2017")
 plot(vfc1017,col=cFCOVER, main="FCover october 2017")
 plot(vfc1117,col=cFCOVER, main="FCover november 2017")
-plot(difN17, col=cdif, main= "variazione NDVI 2017")
-plot(diffc17,col=cdif, main= "variazione FCover 2017")
 
-dev.off()
-
-
-
-
-
-######################## 2019   ##########################
-
-#NDVI_201910010000_GLOBE_PROBAV_V2.2.1	01/10/2019	10/10/2019	213.7 MB
-#NDVI_201911110000_GLOBE_PROBAV_V2.2.1	11/11/2019	20/11/2019	181 MB
-#Vegetation Properties - FCOVER 1km Global V1
-#FCOVER_201910030000_GLOBE_PROBAV_V1.5.1	15/09/2019	15/10/2019
-#FCOVER_201911230000_GLOBE_PROBAV_V1.5.1	05/11/2019	05/12/2019
-
-# we use raster function to charge the satellite images
-N1019<-raster("c_gls_NDVI_201910010000_GLOBE_PROBAV_V2.2.1.nc")
-N1119<-raster("c_gls_NDVI_201911110000_GLOBE_PROBAV_V2.2.1.nc")
-
-#to crop the images zooming on the Veneto region
-vN1019<-crop(N1019, ext)
-vN1119<-crop(N1119, ext)
-
-
-
-#how the NDVIis is changed from october to november 2018
-difN19<- vN1019-vN1119
+#how the NDVIis is changed from october to november 2017
+difN17<- vN1017-vN1117
 par(mfrow=c(1,2))
-plot(difN19, col=cdif)
-hist(difN19, col='green') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
+plot(difN17, col=cdif)
+hist(difN17, col='springgreen', xlab= 'NDVI variation') 
 dev.off()
-levelplot(difN19) 
+levelplot(difN17) 
 
-
-#to see the differences in cover vegetation
-fc1019<- raster("c_gls_FCOVER_201910030000_GLOBE_PROBAV_V1.5.1.nc")
-fc1119<- raster("c_gls_FCOVER_201911230000_GLOBE_PROBAV_V1.5.1.nc")
-
-vfc1019<- crop(fc1019, ext)
-vfc1119<- crop(fc1119, ext)
-
-diffc19 <- vfc1019-vfc1119
+#how the FCOVER is changed from october to november 2017
+diffc17 <- vfc1017-vfc1117
 par(mfrow=c(1,2))
-plot(diffc19, col=cdif)
-hist(diffc19, col='green') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
+plot(diffc17, col=cdif)
+hist(diffc17, col='blue', xlab='FCOVER variation') #to do an istrogramm which plot the frquency of the values,how much pixel have one value?
 dev.off()
-levelplot(diffc19)  
-
-
-par(mfrow=c(3,2)) #3 rows and 2 columns
-plot(vN1019, main="NDVI october 2019")
-plot(vN1119, main="NDVI november 2019")
-plot(vfc1019, main="FCover october 2019")
-plot(vfc1119, main="FCover november 2019")
-plot(difN19, main= "variazione NDVI 2019")
-plot(diffc19, main= "variazione FCover 2019")
+levelplot(diffc17) 
 
 #I want to do a match between the different NDVI changes
 
@@ -257,8 +192,7 @@ plot(vN1017, col=cNDVI, main="NDVI october 2017")
 plot(vN1117, col=cNDVI, main="NDVI november 2017")
 plot(vN1018, col=cNDVI, main="NDVI october 2018")
 plot(vN1118, col=cNDVI, main="NDVI november 2018")
-plot(vN1019, col=cNDVI, main="NDVI october 2019")
-plot(vN1119, col=cNDVI, main="NDVI november 2019")
+
 
 library(rgdal)
 par(mfrow=c(3,1))
@@ -276,8 +210,6 @@ vn0619<-crop(n0619,ext)
 vf0618<-crop(f0618,ext)
 vf0619<-crop(f0619,ext)
 
-n0618<-raster("c_gls_NDVI_201807210000_GLOBE_PROBAV_V2.2.1.nc")
-n0619<-raster("c_gls_NDVI_201908010000_GLOBE_PROBAV_V2.2.1.nc")
 
 par(mfrow=c(2,2))
 plot(vn0618,col=cNDVI,main="NDVI june 2018")
